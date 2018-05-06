fe35f406212fa321183665b68e46902e4ff0f43e
su-kun1899 <higedrum.coz@gmail.com>
2018-05-06 18:56:40 +0900

    円

---
+++ python/draw_circle.py
@@ -0,0 +1,40 @@
+import sys
+
+import pygame
+from pygame.locals import QUIT
+
+pygame.init()
+SURFACE = pygame.display.set_mode((400, 300))
+FPSCLOCKE = pygame.time.Clock()
+
+
+def main():
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+        SURFACE.fill((255, 255, 255))
+
+        # 赤: 塗りつぶし
+        pygame.draw.circle(SURFACE, (255, 0, 0), (50, 50), 20)
+
+        # 赤: 太さ10
+        pygame.draw.circle(SURFACE, (255, 0, 0), (150, 50), 20, 10)
+
+        # 緑: 半径10
+        pygame.draw.circle(SURFACE, (0, 255, 0), (50, 150), 10)
+
+        # 緑: 半径20
+        pygame.draw.circle(SURFACE, (0, 255, 0), (150, 150), 20)
+
+        # 緑: 半径30
+        pygame.draw.circle(SURFACE, (0, 255, 0), (250, 150), 30)
+
+        pygame.display.update()
+        FPSCLOCKE.tick(3)
+
+
+if __name__ == '__main__':
+    main()
